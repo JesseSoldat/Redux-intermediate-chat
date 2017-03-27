28fa3f17ee197ec2ff2b4abf0e9e04229d57e193
jesse <jsoldat@hotmail.com>
2017-03-27 18:07:17 +0700

    added action creators;

--- public/app.js
+++ public/app.js
@@ -79,6 +79,28 @@ function messagesReducer(state = [], action) {
 
 const store = Redux.createStore(reducer);
 
+function deleteMessage(id) {
+	return {
+		type: 'DELETE_MESSAGE',
+		id: id,
+	};
+}
+
+function openThread(id) {
+	return {
+		type: 'OPEN_THREAD',
+		id: id,
+	};
+}
+
+function addMessage(text, threadId) {
+	return {
+		type: 'ADD_MESSAGE',
+		text: text,
+		threadId: threadId,
+	};
+}
+
 
 const App = () => (
 	<div className='ui segment'>
@@ -116,10 +138,7 @@ const mapStateToTabsProps = (state) => {
 const mapDispatchToTabsProps = (dispatch) => (
 	{
 		onClick: (id) => (
-			dispatch({
-				type: 'OPEN_THREAD',
-				id: id,
-			})
+			dispatch(openThread(id))
 		)
 	}
 );
@@ -208,10 +227,7 @@ const mapStateToThreadProps = (state) => (
 const mapDispatchToThreadProps = (dispatch) => (
 	{
 		onMessageClick: (id) => (
-			dispatch({
-				type: 'DELETE_MESSAGE',
-				id: id,
-			})
+			dispatch(deleteMessage(id))
 		),
 		dispatch: dispatch,
 	}
@@ -222,11 +238,9 @@ const mergeThreadProps = (stateProps, dispatchProps) => (
 		...stateProps,
 		...dispatchProps,
 		onMessageSubmit: (text) => (
-			dispatchProps.dispatch({
-				type: 'ADD_MESSAGE',
-				text: text,
-				threadId: stateProps.thread.id,
-			})
+			dispatchProps.dispatch(
+				addMessage(text, stateProps.thread.id)
+			)
 		),
 	}
 );
